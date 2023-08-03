dc9dc8299272adabd3f1142cf74cf72e992d1ae5
Shreya Prasad <shreyapd666@gmail.com>
2023-08-03 18:10:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import Reataurnt from './Component/Basics/Restaurnt'
+import Temp from './Component/weather/temp'
+
+const App = () => {
+  return (
+    <Temp />
+  )
+}
+
+export default App
